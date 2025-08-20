e6f9a234893bd401945495c2091be2e10dc6bc4c
yokhoe <yonathan.khoe@unt.edu>
2025-08-20 17:41:45 -0500

    Started over to be more polished. Got to a working state successfully again.

---
+++ it-beacon-systray/App.xaml.cs
@@ -0,0 +1,69 @@
+﻿using Hardcodet.Wpf.TaskbarNotification;
+using it_beacon_systray.Views;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace it_beacon_systray
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private TaskbarIcon? _notifyIcon;
+        private PopupWindow? _popupWindow;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // preload popup window
+            _popupWindow = new PopupWindow();
+
+            // preload tray icon resource
+            var trayIcon = (TaskbarIcon)FindResource("TrayIcon");
+
+            // load embedded icon from it-beacon-common
+            var iconUri = new Uri("pack://application:,,,/it-beacon-common;component/Assets/Icons/beacon.ico");
+            trayIcon.IconSource = new BitmapImage(iconUri);
+
+            _notifyIcon = trayIcon;
+
+            // wire up events
+            _notifyIcon.TrayLeftMouseUp += (s, ev) => TogglePopup();
+            _notifyIcon.TrayRightMouseUp += (s, ev) => TogglePopup();
+            _notifyIcon.TrayMouseDoubleClick += (s, ev) => OpenNotepad();
+        }
+
+        private void TogglePopup()
+        {
+            if (_popupWindow == null) return;
+
+            if (_popupWindow.IsVisible)
+            {
+                _popupWindow.Hide();
+            }
+            else
+            {
+                _popupWindow.PositionNearTray();
+                _popupWindow.Show();
+                _popupWindow.Activate();
+            }
+        }
+
+        private void OpenNotepad()
+        {
+            Process.Start(new ProcessStartInfo("notepad.exe") { UseShellExecute = true });
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            _notifyIcon?.Dispose();
+            base.OnExit(e);
+        }
+    }
+
+}
---
+++ it-beacon-systray/Views/PopupWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace it_beacon_systray.Views
+{
+    /// <summary>
+    /// Interaction logic for PopupWindow.xaml
+    /// </summary>
+    public partial class PopupWindow : Window
+    {
+        public PopupWindow()
+        {
+            InitializeComponent();
+
+            // Close popup when it loses focus
+            this.Deactivated += (s, e) => this.Hide();
+        }
+
+        /// <summary>
+        /// Positions the popup just above the system tray (bottom-right).
+        /// </summary>
+        public void PositionNearTray()
+        {
+            var workArea = SystemParameters.WorkArea; // usable screen without taskbar
+
+            // Set to bottom-right
+            this.Left = workArea.Right - this.Width - 10; // 10px margin
+            this.Top = workArea.Bottom - this.Height - 10; // 10px margin
+        }
+    }
+}
